74a8ecaa8157f7d85da41aa8b55c600b095e766f
ROG <luan.roger.2003@gmail.com>
2025-11-12 19:42:32 -0300

    refactor: remove the instrumentation

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,11 +1,9 @@
 import { withCMS } from "@repo/cms/next-config";
-import { withToolbar } from "@repo/feature-flags/lib/toolbar";
 import { config, withAnalyzer } from "@repo/next-config";
-import { withLogging, withSentry } from "@repo/observability/next-config";
 import type { NextConfig } from "next";
 import { env } from "@/env";
 
-let nextConfig: NextConfig = withToolbar(withLogging(config));
+let nextConfig: NextConfig = config;
 
 nextConfig.images?.remotePatterns?.push({
   protocol: "https",
@@ -22,10 +20,6 @@ if (process.env.NODE_ENV === "production") {
   ];
 
   nextConfig.redirects = redirects;
-}
-
-if (env.VERCEL) {
-  nextConfig = withSentry(nextConfig);
 }
 
 if (env.ANALYZE === "true") {
