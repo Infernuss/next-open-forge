32eff82cbfa6468e18ea3912be665df93c73d82c
ROG <luan.roger.2003@gmail.com>
2025-11-14 10:23:25 -0300

    refactor: remove web project

--- packages/next-config/keys.ts
+++ packages/next-config/keys.ts
@@ -11,16 +11,10 @@ export const keys = () =>
     },
     client: {
       NEXT_PUBLIC_APP_URL: z.url(),
-      NEXT_PUBLIC_WEB_URL: z.url(),
-      NEXT_PUBLIC_API_URL: z.url().optional(),
-      NEXT_PUBLIC_DOCS_URL: z.url().optional(),
     },
     runtimeEnv: {
       ANALYZE: process.env.ANALYZE,
       NEXT_RUNTIME: process.env.NEXT_RUNTIME,
       NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-      NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-      NEXT_PUBLIC_DOCS_URL: process.env.NEXT_PUBLIC_DOCS_URL,
     },
   });
