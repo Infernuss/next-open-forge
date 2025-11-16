e0174e3d8dc7cb7eb894569cc45ad1b1f5f053d6
ROG <luan.roger.2003@gmail.com>
2025-11-16 16:18:02 -0300

    feat: create user avatar menu

--- apps/app/app/(authenticated)/components/header.tsx
+++ apps/app/app/(authenticated)/components/header.tsx
@@ -6,21 +6,19 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@repo/design-system/components/ui/breadcrumb";
-import { Separator } from "@repo/design-system/components/ui/separator";
 import { SidebarTrigger } from "@repo/design-system/components/ui/sidebar";
 import { Fragment, Suspense } from "react";
 import UserMenu, { UserMenuLoading } from "@/app/components/user-menu";
 
 type HeaderProps = {
   pages: string[];
-  page: string;
+  currentPage: string;
 };
 
-export const Header = ({ pages, page }: HeaderProps) => (
-  <header className="flex h-16 shrink-0 items-center justify-between gap-2">
-    <div className="flex items-center gap-2 px-4">
+export const Header = ({ pages, currentPage }: HeaderProps) => (
+  <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4">
+    <div className="flex items-center gap-2">
       <SidebarTrigger />
-      <Separator className="mr-2 h-4" orientation="vertical" />
       <Breadcrumb>
         <BreadcrumbList>
           {pages.map((page, index) => (
@@ -33,7 +31,7 @@ export const Header = ({ pages, page }: HeaderProps) => (
           ))}
           <BreadcrumbSeparator className="hidden md:block" />
           <BreadcrumbItem>
-            <BreadcrumbPage>{page}</BreadcrumbPage>
+            <BreadcrumbPage>{currentPage}</BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
---
+++ apps/app/app/components/user-menu.tsx
@@ -0,0 +1,45 @@
+import { currentUser, signOut } from "@repo/auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@repo/design-system/components/ui/dropdown-menu";
+import { Skeleton } from "@repo/design-system/components/ui/skeleton";
+import { LogOutIcon } from "lucide-react";
+import UserAvatar from "./user-avatar";
+
+export default async function UserMenu() {
+  const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const username = user.username ?? user.name;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar username={username} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>{user.name}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem onClick={signOut}>
+            <LogOutIcon />
+            Sign Out
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function UserMenuLoading() {
+  return <Skeleton className="size-8 rounded-full" />;
+}
